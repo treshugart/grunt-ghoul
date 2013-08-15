8bb92cc0f9cc54186b60c530bd35e08822ff1855
Trey Shugart <treshugart@gmail.com>
2013-08-15 23:08:07 +1000

    Set up task and tests.

--- tasks/ghoul.js
+++ tasks/ghoul.js
@@ -8,43 +8,114 @@
 
 'use strict';
 
+var runners = {
+  mocha: function(grunt, html) {
+    var $ = require('jquery');
+    var $html = $(html);
+    var duration = $html.find('.duration em').text();
+    var passes = +$html.find('.passes em').text();
+    var failures = +$html.find('.failures em').text();
+
+    grunt.verbose.writeln('  Duration: ' + duration + 's');
+    grunt.log.writeln('  Passes: ' + passes);
+    grunt.log.writeln('  Failures: ' + failures);
+    grunt.log.writeln();
+
+    $html.find('.suite').each(function(a, suite) {
+      var suite = $(suite);
+
+      grunt.log.writeln('  ' + suite.find('h1').contents().eq(0).text());
+
+      $html.find('.test').each(function(i, test) {
+        test = $(test);
+
+        grunt.log.write('    ');
+        grunt.log.write(test.is('.pass') ? '✓' : '✗');
+        grunt.log.write(' ' + test.find('h2').contents().eq(0).text());
+        grunt.verbose.write(' ' + test.find('.duration').text());
+        grunt.log.writeln();
+        grunt.log.writeln();
+      });
+    });
+
+    return failures === 0;
+  }
+};
+
 module.exports = function(grunt) {
+  grunt.registerMultiTask('ghoul', 'An abstract test runner that runs and reports on front-end test suites.', function() {
+    var data = this.data;
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // The user can use their own runner or specify an internal one to use.
+    var runner = typeof data.runner === 'function' ? data.runner : runners[data.runner || 'mocha'];
 
-  grunt.registerMultiTask('ghoul', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var phantom = require('grunt-lib-phantomjs').init(grunt);
+    var done = this.async();
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    // Console logging is used to trigger events in Ghoul from PhantomJS
+    // instead of the default alerts. This is cleaner especially when you also
+    // want to run your tests in the browser without having to detect if it is
+    // PhantomJS running the tests.
+    phantom.on('console', function(msg) {
+      if (msg.indexOf('ghoul.') === 0) {
+        // Match any console log that begins with the task name.
+        var args = msg.match(/(ghoul\.[^\s]+)\s*(.*)/i);
+
+        // The event name that is triggered is the first part of the log.
+        var name = args[1];
+
+        // The second part is a JSON encoded string of arguments to pass as
+        // event data.
+        var args = (args[2] || '').replace(/"/g, '\\"');
+
+        // If the argument wasn't passed as an encoded
+        // array, make it one taking into account if
+        // the type that was passe was a number or
+        // string.
+        if (args.indexOf('[') !== 0) {
+          if (typeof args === 'number') {
+            args = '[' + args + ']'
+          } else {
+            args = '["' + args + '"]'
+          }
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+        args = JSON.parse(args);
+        args.unshift(name);
+        phantom.emit.apply(phantom, args);
+      }
     });
-  });
 
+    phantom.on('ghoul.done', function(html) {
+      // The test runner must return false in order to halt task execution.
+      if (runner(grunt, html) === false) {
+        grunt.fail.warn('Tests failed.');
+      }
+
+      // When the tests are done, we continue to the next test runner.
+      phantom.halt();
+    });
+
+    // Each URL is considered a test runner.
+    grunt.util.async.forEachSeries(
+      data.urls || [],
+      function(url, next) {
+        grunt.log.writeln('Running: ' + url);
+
+        phantom.spawn(url, {
+          options: data.phantom || {},
+          done: function(error) {
+            if (error) {
+              done(error);
+            } else {
+              next();
+            }
+          }
+        });
+      },
+      function() {
+        done();
+      }
+    );
+  });
 };
